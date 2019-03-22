4e8e8cd70ba72f57d8758af0b070a51cb0ee023b
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-03-22 13:38:08 +0100

    Add middleware operations, mark deprecats.

--- src/app.js
+++ src/app.js
@@ -3,16 +3,16 @@ const express = require('@feathersjs/express');
 const configuration = require('@feathersjs/configuration');
 const favicon = require('serve-favicon');
 const path = require('path');
-// const bodyParser = require('body-parser');
-const logger = require('./logger');
+// const bodyParser = require('body-parser'); @deprecated
 
 const hooks = require('./global/');
 const database = require('./database/');
 const services = require('./services/');
-// const middleware = require('./middleware'); // todo test if it is need
+const middleware = require('./middleware');
 
 // const defaultHeaders = require('./middleware/defaultHeaders'); @deprecated
 const handleResponseType = require('./middleware/handleResponseType');
+
 
 const conf = configuration();
 
@@ -20,21 +20,16 @@ const app = express(feathers())
 	.configure(conf)
 	.use(express.json())
 	.use(express.urlencoded({ extended: true }))
-	// todo "handleResponseType" test it, maybe no effect see express.json()
+	// todo "handleResponseType" test it, maybe no effect see express.json() @deprecated
 	.configure(express.rest(handleResponseType))
 
 	.use('/', express.static('public'))
 	.use(favicon(path.join('public', 'favicon.ico')))
 
-	// .use(defaultHeaders) // todo test it, position,  if we need it?
+	// .use(defaultHeaders) // todo test it, position,  if we need it? @deprecated
 
 	.configure(database)
-	// .configure(middleware)
-	.use((req, res, next) => {
-		req.feathers.headers = req.headers;
-		logger.request(req);
-		next();
-	})
+	.configure(middleware)
 	.configure(services)
 	.hooks(hooks)
 	.use(express.errorHandler({
@@ -44,10 +39,8 @@ const app = express(feathers())
 			res.json(error);
 		},
 	}));
-
-app.logger = logger;
 /*
-app.on('unhandledRejection', (reason, p) => {
+app.on('unhandledRejection', (reason, p) => { @deprecated
 	logger.info('Unhandled Rejection at: Promise ', p, ' reason: ', reason);
 });
 */
