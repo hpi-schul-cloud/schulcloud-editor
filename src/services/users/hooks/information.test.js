7a41703bd2b98951289211891fb69171e25fe1ed
Wolfgang Rahtgeb <W.Rathgeb@dropanote.de>
2019-05-08 14:12:49 +0200

    add second test

--- src/services/users/hooks/information.test.js
+++ src/services/users/hooks/information.test.js
@@ -24,7 +24,7 @@ describe('\'user information\' hook', () => {
 		});
 	});
 
-	it('removeIds from object and nested objects', async () => {
+	it('removeIds from object and nested objects 1', async () => {
 		const data = [
 			{
 				_id: {
@@ -89,4 +89,107 @@ describe('\'user information\' hook', () => {
 			return e;
 		}
 	});
+
+	it('removeIds from object and nested objects 2', async () => {
+		const data = [
+			{
+				_id: {
+					_bysontype: 'ObjectID',
+					id: 'jfalsdfp8a9sdfa3fa3dfasda',
+				},
+				owner: {
+					_bysontype: 'ObjectID',
+					id: 'jfalsdfp8a9sdfa3fdfa3dsfasda',
+				},
+				titel: 'test',
+				type: 'lesson',
+				random: 'this is a random value',
+				sections: [
+					{
+						_id: {
+							_bysontype: 'ObjectID',
+							id: 'jfalsdfpd8a9sdfa3ffsdfa3dfasda',
+						},
+						owner: {
+							_bysontype: 'ObjectID',
+							id: 'jfaldsdfp8a9sdsfa3fdfa3dsfasda',
+						},
+						randomOtherId: { 
+							_bysontype: 'ObjectID',
+							id: 'jfaldsd33334234fp8a9sdsfa3fdfa3dsfasda',
+						},
+						titel: 'Nacht und Tag',
+						type: 'section',
+						random: {
+							type: 'ramdom nested value',
+							array: [
+								'random', 'is', 'the', 'new', 'black',
+							],
+						},
+					},
+					{
+						_id: {
+							_bysontype: 'ObjectID',
+							id: 'jfalsdsfp8a9sdfa3ffsdfa3dfasda',
+						},
+						owner: {
+							_bysontype: 'ObjectID',
+							id: 'jfalsdfp8a9sdsfa3fdfa3dsfas4da',
+						},
+						titel: 'tanzen',
+						type: 'section',
+					},
+					{
+						_id: {
+							_bysontype: 'ObjectID',
+							id: 'jfalsdfp8a9sdfa3f23fsdfa3dfasda',
+						},
+						owner: {
+							_bysontype: 'ObjectID',
+							id: 'jfalsdfp8a9sdsfa3fd33fa3dsfasda',
+						},
+						titel: 'Tango',
+						type: 'section',
+					},
+				],
+			},
+		];
+
+		const result = { data };
+
+		try {
+			const cleaned = await app.service(serviceName).get(result);
+
+			assert.deepEqual(cleaned, [
+				{
+					titel: 'test',
+					type: 'lesson',
+					random: 'this is a random value',
+					sections: [
+						{
+							titel: 'Nacht und Tag',
+							type: 'section',
+							random: {
+								type: 'ramdom nested value',
+								array: [
+									'random', 'is', 'the', 'new', 'black',
+								],
+							},
+						},
+						{
+							titel: 'tanzen',
+							type: 'section',
+						},
+						{
+							titel: 'Tango',
+							type: 'section',
+						},
+					],
+				},
+			]);
+		} catch (e) {
+			return e;
+		}
+	});
+
 });
