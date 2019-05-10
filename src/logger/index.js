3ad4423fe25cf1972bdbd2a5458f789ea04c871c
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-05-10 14:53:55 +0200

    add winston syslog

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,14 +1,34 @@
 const event = require('./event');
 const request = require('./request');
+const winston = require('winston');
+require('winston-syslog').Syslog
+const syslogConfigs = require('../../config/syslog.json');
 
-const logger = {
-	error: console.log,
-	warn: console.log,
-	info: console.log,
-};
+const syslogConfig = {};
+if(process.env.NODE_ENV === "productive"){
+	syslogConfig = syslogConfigs.productive;
+
+} else {
+	syslogConfig = syslogConfigs.development;
+}
+
+
+
+const logger = winston.createLogger({
+	levels: winston.config.syslog.levels,
+	transports: [
+		new winston.transports.Syslog(syslogConfig),
+		new winston.transports.Console({
+			handleExceptions: true,
+			json: false
+		  })
+	]
+})
+
 
 // add events log for methods
 logger.event = event(logger, ['create', 'remove', 'update', 'patch']);
 logger.request = request(logger);
+
 
 module.exports = logger;
