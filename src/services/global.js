92687bd958f2149b484341c6b9aa180ab3be6148
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-08-30 10:39:58 +0200

    empty services folder

--- src/services/global.js
+++
@@ -1,9 +0,0 @@
-const helpers = require('../global/helpers');
-const hooks = require('../global/hooks');
-const models = require('./models');
-
-module.exports = {
-	helpers,
-	hooks,
-	models,
-};
--- src/services/users/index.js
+++
@@ -1,6 +0,0 @@
-// todo later
-/*
-this service can try with get to collect all informations for a userId
-and try remove to delete all this information
-it will throw an event and the services have added the operationens for it.
-*/
