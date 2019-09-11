7a2294434b1f00a8123bef014a3fed2a5e73d65d
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-09-11 16:50:55 +0200

    clear unused global hooks

--- src/global/hooks.js
+++ src/global/hooks.js
@@ -1,72 +1,9 @@
 /* eslint-disable no-param-reassign */
-const { NotFound, MethodNotAllowed, Forbidden } = require('@feathersjs/errors');
+const { MethodNotAllowed, Forbidden } = require('@feathersjs/errors');
 const axios = require('axios');
-
-const { forceArray } = require('./helpers');
-
-const objectPathResolver = (context, path) => {
-	if (path === undefined) {
-		return context;
-	}
-
-	return path.split('.').reduce((contextElement, key) => {
-		const next = contextElement[key];
-		return next;
-	}, context);
-};
-
-const filter = (path, keys) => (context) => {
-	console.log('todo: filter');
-	if (keys === undefined) {
-		return context;
-	}
-
-	const copy = Object.assign({}, context);
-	return copy;
-};
-
-const throwNotFoundIfTrue = (context, bool, path, keys) => {
-	if (bool === true) {
-		throw new NotFound('Missing input value', { path, keys });
-	}
-	return context;
-};
-
-const exist = (path, keys, executer = throwNotFoundIfTrue) => (context) => {
-	if (keys === undefined) {
-		return context;
-	}
-
-	const target = objectPathResolver(context, path);
-	const isNotFound = forceArray(keys).some(key => target[key] === undefined);
-
-	return executer(context, isNotFound, path, keys);
-};
-
-
-// todo restricted to groups ..populate id?
-
 
 const block = () => {
 	throw new MethodNotAllowed();
-};
-
-const populate = paths => (context) => {
-	if (!context.params.query.$populate) {
-		context.params.query.$populate = [];
-	}
-	forceArray(paths).forEach((path) => {
-		context.params.query.$populate.push({ path });
-	});
-
-	return context;
-};
-
-const forceOwnerInData = (context) => {
-	if (!context.data.owner) {
-		context.data.owner = context.params.user;
-	}
-	return context;
 };
 
 /* eslint-disable no-param-reassign */
@@ -97,9 +34,19 @@ const filterOutResults = keys => (context) => {
  * @param {string} permission
  */
 const checkCoursePermission = permission => async (context) => {
-	const { params: { user, route: { courseId }, authorization }, app } = context;
+	const {
+		params: {
+			user, provider, route: { courseId }, authorization,
+		},
+		app,
+	} = context;
+
+	if (!(provider === 'rest')) {
+		return context;
+	}
+
 	const { api, coursePermissions } = app.get('routes');
-	const baseURL = (api + coursePermissions).replace(':courseId', courseId);
+	const baseURL = (api + coursePermissions).replace(':courseId', courseId); // todo wrapper in routes that can request over get(couseId)?
 	const coursePermissionServices = axios.create({
 		baseURL,
 		timeout: 4000,
@@ -124,12 +71,7 @@ const checkCoursePermission = permission => async (context) => {
 };
 
 module.exports = {
-	// exist,
-	// filter,
-	// objectPathResolver,
 	block,
-	// populate,
-	// forceOwnerInData,
 	filterOutResults,
 	checkCoursePermission,
 };
