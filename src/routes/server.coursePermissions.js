120d737b289c0ef1dde004edbae703564475f358
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2019-09-13 14:45:38 +0200

    change user to user.id

--- src/routes/server.coursePermissions.js
+++ src/routes/server.coursePermissions.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const coursePermissions = (app, courseId, userId, authorization) => {
 	const { server: { baseUrl, coursePermissionsUri }, timeout } = app.get('routes');
 	const url = (baseUrl + coursePermissionsUri).replace(':courseId', courseId);
+	// TODO: define gloabl - ticket EDTR-150
 	const coursePermissionServices = axios.create({
 		baseURL: url,
 		timeout,
--- src/services/permissionsHelper/hooks/baseServicesAccess.js
+++ src/services/permissionsHelper/hooks/baseServicesAccess.js
@@ -8,7 +8,7 @@ const baseServicesAccess = (shortPath, testedAccess) => async (context) => {
 	const path = `${shortPath}/${testedAccess}`;
 	const copyParams = Object.assign({}, params);
 	copyParams.route.ressourceId = context.id;
-	const { access } = await app.service(path).get(params.user, copyParams);
+	const { access } = await app.service(path).get(params.user.id, copyParams);
 	if (!access) {
 		throw new Forbidden('You have no access.');
 	}
