2f841a459588b520cc684dd5cf8a3501eb08240b
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-09-23 09:28:42 +0200

    Fix condition.

--- src/services/permissionsHelper/hooks/restrictedPermWithoutPatch.js
+++ src/services/permissionsHelper/hooks/restrictedPermWithoutPatch.js
@@ -2,7 +2,7 @@ const { Forbidden } = require('@feathersjs/errors');
 
 const restrictedPermWithoutPatch = (context) => {
 	const { query, provider } = context.params;
-	if (!query.disabledPatch && provider === 'rest') {
+	if (query.disabledPatch && provider === 'rest') {
 		throw new Forbidden('You have no access.');
 	}
 	return context;
