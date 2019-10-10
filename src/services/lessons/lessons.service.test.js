c3d266ce5fc77ea21fb946c27ed392b91ac61dc6
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2019-10-10 14:19:01 +0200

    add lesson patch tests

--- src/services/lessons/lessons.service.test.js
+++ src/services/lessons/lessons.service.test.js
@@ -59,7 +59,7 @@ describe.only('lessons/lessons.service.js', () => {
 
 		expect(status).to.equal(200);
 		expect(data).to.an('object');
-		expect(data._id).to.equal(data._id.toString());
+		expect(data._id).to.equal(id.toString());
 		expect(data.title).to.equal(title);
 		expect(data.courseId).to.equal(courseId);
 		expect(data.sections).to.have.lengthOf(0);
@@ -75,7 +75,7 @@ describe.only('lessons/lessons.service.js', () => {
 
 		expect(status).to.equal(200);
 		expect(data).to.an('object');
-		expect(data._id).to.equal(data._id.toString());
+		expect(data._id).to.equal(id.toString());
 		expect(data.title).to.equal(title);
 		expect(data.courseId).to.equal(courseId);
 		expect(data.sections).to.have.lengthOf(0);
@@ -101,5 +101,50 @@ describe.only('lessons/lessons.service.js', () => {
 		expect(data.data).to.have.lengthOf(2);
 		expect(data.data[0]).to.not.have.property('permissions');
 	});
-	// todo find sortierung
+
+	// todo find sortierung and paginations
+
+	it('patch with write permission should work', async () => {
+		const writePermission = helper.createPermission({ users: [userId], write: true });
+		const lesson = await service.createWithDefaultData(writePermission, { courseId });
+		const { _id: id } = lesson;
+
+		const patchedData = {
+			title: 'WUSA 512',
+		};
+		const { status, data } = await service.sendRequestToThisService('patch', {
+			id, userId, courseId, data: patchedData,
+		});
+
+		const { status: getStatus, data: getData } = await service.sendRequestToThisService('get', {
+			id, userId, courseId,
+		});
+
+		expect(status).to.equal(200);
+		expect(data).to.an('object');
+		expect(data._id).to.equal(id.toString());
+		expect(Object.keys(data)).to.have.lengthOf(1);
+
+		expect(getStatus).to.equal(200);
+		expect(getData).to.an('object');
+		expect(getData._id).to.equal(id.toString());
+		expect(getData.title).to.equal(patchedData.title); // patched
+		expect(getData.courseId).to.equal(courseId); // not patched
+		expect(getData.sections).to.have.lengthOf(0);
+	});
+
+	it('patch with read permission should not work', async () => {
+		const readPermission = helper.createPermission({ users: [userId], read: true });
+		const lesson = await service.createWithDefaultData(readPermission, { courseId });
+		const { _id: id } = lesson;
+
+		const patchedData = {
+			position: 2,
+		};
+		const { status } = await service.sendRequestToThisService('patch', {
+			id, userId, courseId, data: patchedData,
+		});
+
+		expect(status).to.equal(403);
+	});
 });
