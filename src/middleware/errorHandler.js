402da88c3dde509f8595a99919dd539bd37fe606
CeEv <cedric@it-4d.de>
2019-12-03 09:49:56 +0100

    Skip 405 errors

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,16 +1,31 @@
 const express = require('@feathersjs/express');
 const Sentry = require('@sentry/node');
 
-const { systemInfo, requestInfo, redInfo, logger } = require('../logger');
+const {
+	systemInfo, requestInfo, redInfo, logger,
+} = require('../logger');
 
 const executeWithSystemInfo = app => (middleware, info) => {
 	if (info) systemInfo(`[middleware-error] ${info}`);
 	app.use(middleware);
 };
 
-const logError = (error, req, res, next) => {
+const shortErrorLine = (req, error) => {
+	redInfo(`(${error.code}) Route: ${req.path} - ${error.message}`);
+};
+
+const catch405 = (error, req, res, next) => {
+	if (error && error.code === 405) {
+		shortErrorLine(req, error);
+		res.json(error);
+	} else {
+		next(error); // next without error
+	}
+};
+
+const logRelatedErrors = (error, req, res, next) => {
 	if (error) {
-		redInfo(`(${error.code}) Route: ${req.path} - ${error.message}`);
+		shortErrorLine(req, error);
 		logger.error(error);
 	}
 	next(error);
@@ -18,7 +33,6 @@ const logError = (error, req, res, next) => {
 
 const clearErrorsForSendOut = (error, req, res, next) => {
 	if (error) {
-		// app.logger.info(error.stack);
 		// clear for extern
 		delete error.stack;
 		delete error.data;
@@ -44,8 +58,12 @@ module.exports = (app) => {
 	systemInfo('\nAdd error handling middleware:');
 	const exec = executeWithSystemInfo(app);
 
+	exec(catch405, 'If a services do not support an endpoint a error if throw but should not go to error pipeline.');
+
 	if (app.get('NODE_ENV') !== 'test') {
-		exec(logError, 'logError: For pass error to winston logger and set additional short line for seperate errors.');
+		exec(logRelatedErrors,
+			'logError: For pass error to winston logger and set additional short line for seperate errors.'
+			+ 'Do not execute for 405 errors.');
 		exec(logRequestErrors, 'logRequestErrors: Add request by error.');
 		exec(Sentry.Handlers.errorHandler(), 'Sentry.Handlers.errorHandler(): Add sentry error handler.');
 	}
