608edfac5037bd09e3786b75c6cfecc64ad56854
BlackGhostHD <36112357+BlackGhostHD@users.noreply.github.com>
2020-02-04 17:48:05 +0100

    added & tested feathers-sync

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -7,6 +7,7 @@ const { apiJwtHandler } = require('./handleJWTAndAddToContext');
 const addHeaderToContext = require('./addHeaderToContext');
 const aggregateAppVars = require('./aggregateAppVars');
 const socket = require('./socket');
+const feathersSync = require('./feathersSync');
 const sentry = require('./sentry');
 
 
@@ -30,6 +31,7 @@ module.exports = function setup(app) {
 	}
 	exec(aggregateAppVars, 'aggregateAppVars: Add aggregate app vars and display it.'); // TODO: no middleware
 	exec(socket, 'socket: Add socket connections');
+	exec(feathersSync, 'feathers-sync: connected');
 	exec(sentry, 'sentry: Add sentry for logging errors.');
 
 	systemInfo('\n******************************************************\n');
