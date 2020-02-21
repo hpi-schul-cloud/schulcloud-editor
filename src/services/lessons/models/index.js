bf950e051897da656866e9d89d5d93e087dae7fb
Evers <Cedric.Evers@hpi.uni-potsdam.de>
2020-02-21 11:59:10 +0100

    linter and remove unused model key

--- src/services/lessons/models/index.js
+++ src/services/lessons/models/index.js
@@ -13,9 +13,6 @@ const lessonSchema = new Schema({
 	deletedAt: { type: Date, expires: (60 * 60 * 24 * 30) },
 	createdBy: { type: Schema.Types.ObjectId },
 	updatedBy: { type: Schema.Types.ObjectId },
-	// @deprecated
-	// todo first step for imigrate old data, second move it to activated = true / false in permissions group
-	visible: { type: Boolean, default: true },
 	position: { type: Number, default: Date.now },
 	fork: { type: Schema.Types.ObjectId }, // is forked from
 }, {
