735acad3dd34c7692b2a70ea70d63a0c9ab19530
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-03-05 09:40:30 +0100

    update secrets

--- deploy/decryptSecrets.sh
+++ deploy/decryptSecrets.sh
@@ -1,4 +1,4 @@
 echo "decrypt secrets"
 
-openssl aes-256-cbc -K $encrypted_b7461320c5f4_key -iv $encrypted_b7461320c5f4_iv -in travis_rsa.enc -out travis_rsa -d
+openssl aes-256-cbc -K $encrypted_dd6f15d73ffd_key -iv $encrypted_dd6f15d73ffd_iv -in travis_rsa.enc -out travis_rsa -d
 chmod 600 travis_rsa
